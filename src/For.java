5e50e7f32f84a4eab7798c0567f9377b8c1781ec
Nikus2005 <dvdmail2005@mail.ru>
2026-09-25 15:17:23 +0300

    Удалена лишняя папка src

--- src/For.java
+++
@@ -1,7 +0,0 @@
-public class For {
-    public static void main(String[] args) {
-        for(int j=0;j<10;j++){
-            System.out.println(j);
-        }
-    }
-}
--- src/Strings.java
+++
@@ -1,6 +0,0 @@
-public class Strings {
-    public static void main(String[] args) {
-        String string = "Hello World";
-        System.out.println(string);
-    }
-}
